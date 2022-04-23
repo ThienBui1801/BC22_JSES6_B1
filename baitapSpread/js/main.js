f5c9ebe463429c5dfb70b79e994c4c4bb3645f22
ThienBui1801 <buiquythien1801@gmail.com>
2022-04-23 23:12:16 +0700

    done

--- baitapSpread/js/main.js
+++ baitapSpread/js/main.js
@@ -1,4 +1,20 @@
 'use strict';
 
-const text = document.querySelector('heading');
-console.log(text)
+
+const hoverText = () => {
+  let content = '';
+  
+  let text = document.getElementsByClassName("heading")[0].textContent;
+  
+  const arrText = [...text];
+  for (let i = 0; i < arrText.length; i++) {
+    let item = arrText[i];
+    content += `
+      <span>${item}</span>
+    `
+  }
+  document.getElementsByClassName('heading')[0].innerHTML = content;
+
+}
+
+hoverText();
